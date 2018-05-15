93a68f563ee650ba855fc37c994388034c8cdf3c
sebseb7 <sebgreenbus@gmail.com>
2018-05-15 19:10:11 +0200

    list

--- website/config.js
+++ website/config.js
@@ -21,19 +21,6 @@ var themeCss = "themes/default-theme.css";
 
 var networkStat = {
   "tube": [
-	["pool.ipbc.io", "pool_ipbc_io"],
-	["support.ipbc.io", "support_ipbc_io"],
-	["support2.ipbc.io", "support2_ipbc_io"],
-	["ipbc.fatpanda.club", "ipbc_fatpanda_club"],
-	["cryptoknight.cc/ipbc", "cryptoknight_cc"],
-	["youpool.io", "youpool_io"],
-	["ipbc.minez.ro","ipbc_minez_ro"],
-	["pool.ipbc.io","pool_ipbc_io"],
-	["ipbccoin.newpool.pw","ipbccoin_newpool_pw"],
-	["ipbc.bathmatminingpools.com","ipbc_bathmatminingpools_com"],
-	["ipbc.pool.gntl.co.uk","ipbc_pool_gntl_co_uk"],
-	["ipbcpool.supportcryptonight.com","ipbcpool_supportcryptonight_com"],
-	["ipbc.semipool.com","ipbc_semipool_com"],
-	["ipbc.iccob.com","ipbc_iccob_com"]
+	["mining.bit.tube", "https://mining.bit.tube/api"]
   ]
 };
